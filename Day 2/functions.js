17ed329c4009afc42f323e3d3483573b5244cfad
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-12-21 21:48:25 +0600

    loops and ternary operators added

---
+++ Day 2/functions.js
@@ -0,0 +1,15 @@
+
+
+// function addition(a , b) {
+//     return a + b
+// }
+//
+// document.write(addition(10, 20))
+
+
+let number = Number(prompt('Enter the number: '))
+function power(num) {
+    return num * num
+}
+
+document.write('<br> Square of the number :' + power(number));
---
+++ Day 2/ternary_operator.js
@@ -0,0 +1,6 @@
+let a = 20;
+let b = 22;
+
+a > b ? document.write(a, ">", b) : document.write(a, "<", b);
+
+
