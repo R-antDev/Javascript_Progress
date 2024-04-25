230b954f75483253b5f79fb5dccbb7c4890549fb
Shariar Hasan Emon <imemon.sh@gmail.com>
2024-04-25 10:41:25 +0600

    exercise added

--- Day 1/increment_decrement.js
+++
@@ -1,8 +0,0 @@
-// increment and decrement
-
-a = 20
-
-aIncrement = a++
-
-console.log(a)
-console.log(aIncrement)
--- Day 2/ternary_operator.js
+++
@@ -1,6 +0,0 @@
-let a = 20;
-let b = 22;
-
-a > b ? document.write(a, ">", b) : document.write(a, "<", b);
-
-
